5e5a272d08796891d8a8b40e5f1ed4049017cc50
Nodrance <94304824+Nodrance@users.noreply.github.com>
2026-06-12 11:53:00 -0230

    roll directions into a loop

--- src/main.rs
+++ src/main.rs
@@ -85,134 +85,44 @@ fn main() {
     });
 
     // Movement
-
-    let enabled_copy = Arc::clone(&current_enabled);
-    let modes_copy = Arc::clone(&current_modes);
-    let up_copy = Arc::clone(&is_up_pressed);
-    mouse_settings.keys.movement.up.blockable_bind(move || {
-        if enabled_copy.load(Ordering::Relaxed) {
-            let mut movement = MouseMotion::new(
-                    modes_copy.lock().unwrap().get_mode(),
-                    Direction::Up);
-            let enabled_copy_copy = Arc::clone(&enabled_copy);
-            let up_copy_copy = Arc::clone(&up_copy);
-            thread::spawn(move || {
-                let swap_result = up_copy_copy.compare_exchange(false, true, Ordering::AcqRel, Ordering::Acquire);
-                if swap_result.is_err() {return}
-                while
-                    up_copy_copy.load(Ordering::Relaxed) &&
-                    enabled_copy_copy.load(Ordering::Relaxed) {
-                    movement.increment_speed();
-                    let (dx, dy) = movement.get_delta();
-                    inputbot::MouseCursor::move_rel(dx, dy);
-                    thread::sleep(Duration::from_millis(FRAMETIME));
-                }
-            });
-            BlockInput::Block
-        } else {
-            BlockInput::DontBlock
-        }
-    });
-    let up_copy = Arc::clone(&is_up_pressed);
-    mouse_settings.keys.movement.up.bind_release( move || {
-        up_copy.store(false, Ordering::SeqCst);
-    });
-
-    let enabled_copy = Arc::clone(&current_enabled);
-    let modes_copy = Arc::clone(&current_modes);
-    let down_copy = Arc::clone(&is_down_pressed);
-    mouse_settings.keys.movement.down.blockable_bind(move || {
-        if enabled_copy.load(Ordering::Relaxed) {
-            let mut movement = MouseMotion::new(
-                    modes_copy.lock().unwrap().get_mode(),
-                    Direction::Down);
-            let enabled_copy_copy = Arc::clone(&enabled_copy);
-            let down_copy_copy = Arc::clone(&down_copy);
-            thread::spawn(move || {
-                let swap_result = down_copy_copy.compare_exchange(false, true, Ordering::AcqRel, Ordering::Acquire);
-                if swap_result.is_err() {return}
-                while
-                    down_copy_copy.load(Ordering::Relaxed) &&
-                    enabled_copy_copy.load(Ordering::Relaxed) {
-                    movement.increment_speed();
-                    let (dx, dy) = movement.get_delta();
-                    inputbot::MouseCursor::move_rel(dx, dy);
-                    thread::sleep(Duration::from_millis(FRAMETIME));
-                }
-            });
-            BlockInput::Block
-        } else {
-            BlockInput::DontBlock
-        }
-    });
-    let down_copy = Arc::clone(&is_down_pressed);
-    mouse_settings.keys.movement.down.bind_release( move || {
-        down_copy.store(false, Ordering::SeqCst);
-    });
-
-    let enabled_copy = Arc::clone(&current_enabled);
-    let modes_copy = Arc::clone(&current_modes);
-    let left_copy = Arc::clone(&is_left_pressed);
-    mouse_settings.keys.movement.left.blockable_bind(move || {
-        if enabled_copy.load(Ordering::Relaxed) {
-            let mut movement = MouseMotion::new(
-                    modes_copy.lock().unwrap().get_mode(),
-                    Direction::Left);
-            let enabled_copy_copy = Arc::clone(&enabled_copy);
-            let left_copy_copy = Arc::clone(&left_copy);
-            thread::spawn(move || {
-                let swap_result = left_copy_copy.compare_exchange(false, true, Ordering::AcqRel, Ordering::Acquire);
-                if swap_result.is_err() {return}
-                while
-                    left_copy_copy.load(Ordering::Relaxed) &&
-                    enabled_copy_copy.load(Ordering::Relaxed) {
-                    movement.increment_speed();
-                    let (dx, dy) = movement.get_delta();
-                    inputbot::MouseCursor::move_rel(dx, dy);
-                    thread::sleep(Duration::from_millis(FRAMETIME));
-                }
-            });
-            BlockInput::Block
-        } else {
-            BlockInput::DontBlock
-        }
-    });
-    let left_copy = Arc::clone(&is_left_pressed);
-    mouse_settings.keys.movement.left.bind_release( move || {
-        left_copy.store(false, Ordering::SeqCst);
-    });
-
-    let enabled_copy = Arc::clone(&current_enabled);
-    let modes_copy = Arc::clone(&current_modes);
-    let right_copy = Arc::clone(&is_right_pressed);
-    mouse_settings.keys.movement.right.blockable_bind(move || {
-        if enabled_copy.load(Ordering::Relaxed) {
-            let mut movement = MouseMotion::new(
-                    modes_copy.lock().unwrap().get_mode(),
-                    Direction::Right);
-            let enabled_copy_copy = Arc::clone(&enabled_copy);
-            let right_copy_copy = Arc::clone(&right_copy);
-            thread::spawn(move || {
-                let swap_result = right_copy_copy.compare_exchange(false, true, Ordering::AcqRel, Ordering::Acquire);
-                if swap_result.is_err() {return}
-                while
-                    right_copy_copy.load(Ordering::Relaxed) &&
-                    enabled_copy_copy.load(Ordering::Relaxed) {
-                    movement.increment_speed();
-                    let (dx, dy) = movement.get_delta();
-                    inputbot::MouseCursor::move_rel(dx, dy);
-                    thread::sleep(Duration::from_millis(FRAMETIME));
-                }
-            });
-            BlockInput::Block
-        } else {
-            BlockInput::DontBlock
-        }
-    });
-    let right_copy = Arc::clone(&is_right_pressed);
-    mouse_settings.keys.movement.right.bind_release( move || {
-        right_copy.store(false, Ordering::SeqCst);
-    });
+    for (pressed, key, direction) in [
+        (is_up_pressed, mouse_settings.keys.movement.up, Direction::Up),
+        (is_down_pressed, mouse_settings.keys.movement.down, Direction::Down),
+        (is_left_pressed, mouse_settings.keys.movement.left, Direction::Left),
+        (is_right_pressed, mouse_settings.keys.movement.right, Direction::Right),
+    ] {
+        let enabled_copy = Arc::clone(&current_enabled);
+        let modes_copy = Arc::clone(&current_modes);
+        let pressed_copy = Arc::clone(&pressed);
+        key.blockable_bind(move || {
+            if enabled_copy.load(Ordering::Relaxed) {
+                let mut movement = MouseMotion::new(
+                        modes_copy.lock().unwrap().get_mode(),
+                        direction);
+                let enabled_copy_copy = Arc::clone(&enabled_copy);
+                let pressed_copy_copy = Arc::clone(&pressed_copy);
+                thread::spawn(move || {
+                    let swap_result = pressed_copy_copy.compare_exchange(false, true, Ordering::AcqRel, Ordering::Acquire);
+                    if swap_result.is_err() {return}
+                    while
+                        pressed_copy_copy.load(Ordering::Relaxed) &&
+                        enabled_copy_copy.load(Ordering::Relaxed) {
+                        movement.increment_speed();
+                        let (dx, dy) = movement.get_delta();
+                        inputbot::MouseCursor::move_rel(dx, dy);
+                        thread::sleep(Duration::from_millis(FRAMETIME));
+                    }
+                });
+                BlockInput::Block
+            } else {
+                BlockInput::DontBlock
+            }
+        });
+        let pressed_copy = Arc::clone(&pressed);
+        key.bind_release( move || {
+            pressed_copy.store(false, Ordering::SeqCst);
+        });
+    }
 
     // Activation
 
