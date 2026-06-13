1d172ff0e40c21e27b28a8feb24523ddc6f49b3c
Nodrance <94304824+Nodrance@users.noreply.github.com>
2026-06-12 21:42:59 -0230

    add different mouse library, just to see

--- src/main1.rs
+++ src/main1.rs
@@ -1,7 +1,6 @@
 use inputbot::{BlockInput, MouseButton, MouseCursor, MouseWheel};
 use std::{thread, time::Duration};
 use std::sync::{Arc, Mutex, atomic::{AtomicBool, AtomicIsize, Ordering}};
-use mouse_rs::Mouse;
 
 mod settings;
 mod motion;
@@ -159,26 +158,13 @@ fn main() {
 
     let offset_copy = Arc::clone(&current_remaining_offset);
     thread::spawn(move || {
-        let mouse = Mouse::new();
         loop {
             let dx = offset_copy.0.swap(0, Ordering::Relaxed);
             let dy = offset_copy.1.swap(0, Ordering::Relaxed);
-            // MouseCursor::move_rel(
-            //     dx.try_into().unwrap_or_default(),
-            //     dy.try_into().unwrap_or_default(),
-            // );
-            match mouse.get_position() {
-                Ok(curr_pos) =>  {
-                    let targ_x = curr_pos.x + dx as i32;
-                    let targ_y = curr_pos.y + dy as i32;
-                    if let Err(err) = mouse.move_to(targ_x, targ_y) {
-                        println!("Problem moving mouse: {}", err)
-                    }
-                }
-                Err(err) => {
-                    println!("Problem getting position: {}", err)
-                }   
-            }
+            MouseCursor::move_rel(
+                dx.try_into().unwrap_or_default(),
+                dy.try_into().unwrap_or_default(),
+            );
             thread::sleep(Duration::from_millis(FRAMETIME));
         }
     });
