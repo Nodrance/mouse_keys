62857f9cad81562cf330e78dad8428c4c2ab8f4b
Nodrance <94304824+Nodrance@users.noreply.github.com>
2026-06-13 21:34:19 -0230

    fix potential edge case bug

--- src/main.rs
+++ src/main.rs
@@ -158,26 +158,29 @@ fn main() {
     // Actually move
 
     let offset_copy = Arc::clone(&current_remaining_offset);
+    let enabled_copy = Arc::clone(&is_mouse_keys_enabled);
     thread::spawn(move || {
         let mouse = Mouse::new();
         loop {
-            let dx = offset_copy.0.swap(0, Ordering::Relaxed);
-            let dy = offset_copy.1.swap(0, Ordering::Relaxed);
-            // MouseCursor::move_rel(
-            //     dx.try_into().unwrap_or_default(),
-            //     dy.try_into().unwrap_or_default(),
-            // );
-            match mouse.get_position() {
-                Ok(curr_pos) =>  {
-                    let targ_x = curr_pos.x + dx as i32;
-                    let targ_y = curr_pos.y + dy as i32;
-                    if let Err(err) = mouse.move_to(targ_x, targ_y) {
-                        println!("Problem moving mouse: {}", err)
+            if enabled_copy.load(Ordering::Relaxed) {
+                let dx = offset_copy.0.swap(0, Ordering::Relaxed);
+                let dy = offset_copy.1.swap(0, Ordering::Relaxed);
+                // MouseCursor::move_rel(
+                //     dx.try_into().unwrap_or_default(),
+                //     dy.try_into().unwrap_or_default(),
+                // );
+                match mouse.get_position() {
+                    Ok(curr_pos) =>  {
+                        let targ_x = curr_pos.x + dx as i32;
+                        let targ_y = curr_pos.y + dy as i32;
+                        if let Err(err) = mouse.move_to(targ_x, targ_y) {
+                            println!("Problem moving mouse: {}", err)
+                        }
                     }
+                    Err(err) => {
+                        println!("Problem getting position: {}", err)
+                    }   
                 }
-                Err(err) => {
-                    println!("Problem getting position: {}", err)
-                }   
             }
             thread::sleep(Duration::from_millis(FRAMETIME));
         }
